7484f34a17e0448c7dc80378c6831658550fbd95
malhadbah <malhadbah@mail.csuchico.edu>
2017-10-20 17:18:09 -0700

    stars latest commit

--- main.cpp
+++ main.cpp
@@ -8,34 +8,30 @@ int main()
 vector<vector <char> >matrix;
 
 int rows, cols;
-cout<<"enter number of rows and columns of stars : ";
+cout<<"Enter rows and columns of stars:";
 cin>>rows;
 cin>>cols;
 
 //if row and column is invalid print nothing
-if(rows<=0 || cols<=0)
+if(rows>=0 && cols>=0)
 {
 
-//Matrix Dimension
-//Matrix.resize(rows);
-
-for(int row=0; row<rows; row++)
-{
-matrix[rows].resize(cols);
 
 //now the matrix row and col is set
 for(int row=0; row<rows; row++)
  {
+ matrix.resize(rows);
   for(int col=0; col<cols; col++)
-{   //set the stars *
-   matrix[row][col]='*';
-  cout<<matrix[row][cols];
-  cout<<"*"<<endl;
- }
- cout<<endl;
- }
- }
- }
+{
+matrix[row].resize(cols);
+matrix[row][col]='*';
+cout<<matrix[row][col];
+}
+cout<<endl;
+}
+}
+
+
  return 0;
  }
 
